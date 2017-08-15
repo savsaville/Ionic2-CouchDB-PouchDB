134aef7ecf9c91f62c467c423a8afd1ed3fb5306
Richard Saville <richardasaville@gmail.com>
2017-08-15 13:33:13 +1000

    minor changes to module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { TodosProvider } from '../providers/todos/todos';
+import { BrowserModule } from '@angular/platform-browser';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
 
 @NgModule({
   declarations: [
@@ -10,13 +13,17 @@ import { TodosProvider } from '../providers/todos/todos';
     HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    BrowserModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage
   ],
-  providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }, TodosProvider]
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler }, TodosProvider]
 })
 export class AppModule { }
