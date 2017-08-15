8a78c454e99cb3538e4abf569a9ef35e15e91e4c
Richard Saville <richardasaville@gmail.com>
2017-08-15 11:25:11 +1000

    import Todos service, load data, create methods

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { TodosProvider } from '../../providers/todos/todos';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,79 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  todos:any;
 
+  constructor(public navCtrl: NavController, public todoService: TodosProvider, public alertCtrl: AlertController) {
+
+  }
+
+  ionViewDidLoad(){
+
+    this.todoService.getTodos().then((data) => {
+      this.todos = data;
+    });
+
+  }
+
+   createTodo(){
+ 
+    let prompt = this.alertCtrl.create({
+      title: 'Add',
+      message: 'What do you need to do?',
+      inputs: [
+        {
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.todoService.createTodo({title: data.title});
+          }
+        }
+      ]
+    });
+ 
+    prompt.present();
+ 
+  }
+
+  updateTodo(todo){
+ 
+    let prompt = this.alertCtrl.create({
+      title: 'Edit',
+      message: 'Change your mind?',
+      inputs: [
+        {
+          name: 'title'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel'
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.todoService.updateTodo({
+              _id: todo._id,
+              _rev: todo._rev,
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+ 
+    prompt.present();
+  }
+
+   deleteTodo(todo){
+    this.todoService.deleteTodo(todo);
   }
 
 }
